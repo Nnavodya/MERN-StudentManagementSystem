aba49b273de06800ab92dc599562a1e585e931f1
Nnavodya <rajapakse22030.kln@gmail.com>
2026-05-26 10:30:54 +0530

    feat: add useState hook and increment button to CounterFunction component

--- frontend/src/components/CounterFunction.js
+++ frontend/src/components/CounterFunction.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React , {useState} from "react";
 import './App.css';
 import CounterClass from './components/counterClass';
 
 function CounterFunction() {
+
+  let[number,setNumber] = useState(0); //useState is a React hook that allows us to add state to functional components. It returns an array with two elements: the current state value (number) and a function to update that state (setNumber). We initialize the state with 0, which means the counter will start at 0.
   return (
     <div>
-      <h1>Counter Function Component</h1> 
+      <h3>Functional Component</h3>
+      <h1>Counter = {number}</h1>
+      <button>Increment</button>
+
       </div>
   );
 }
