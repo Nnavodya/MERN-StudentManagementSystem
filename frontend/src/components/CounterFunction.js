cfbbb8acdfbb1ece6d9c0f75843bd4431f02f3b1
Nnavodya <rajapakse22030.kln@gmail.com>
2026-05-26 11:22:28 +0530

    feat: add increment function with onClick handler in CounterFunction

--- frontend/src/components/CounterFunction.js
+++ frontend/src/components/CounterFunction.js
@@ -1,15 +1,17 @@
 import React , {useState} from "react";
-import '../App.css';
-import CounterClass from './counterClass';
 
 function CounterFunction() {
 
   let[number,setNumber] = useState(0); //useState is a React hook that allows us to add state to functional components. It returns an array with two elements: the current state value (number) and a function to update that state (setNumber). We initialize the state with 0, which means the counter will start at 0.
+  
+  function increment() {
+    setNumber(++number ); //setNumber is the function that we use to update the state. We call it with the new value of number, which is the current value of number plus 1. This will update the counter value in the state, which will trigger a re-render of the component to reflect the new counter value in the UI.
+  }
   return (
     <div>
       <h3>Functional Component</h3>
       <h1>Counter = {number}</h1>
-      <button>Increment</button>
+      <button onClick={increment}>Increment</button>
 
       </div>
   );
