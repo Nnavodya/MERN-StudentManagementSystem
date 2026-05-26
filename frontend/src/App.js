2093d68daf15a224ae55e86a12dd93215ec5b6f4
Nnavodya <rajapakse22030.kln@gmail.com>
2026-05-26 10:17:29 +0530

    feat: add CounterFunction functional component with basic JSX structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 //When using import counterClass from './components/counterClass' ,don't put counterClass.js because it will automatically look for the file with the name counterClass.js in the components folder. If we put counterClass.js it will give an error because it will look for a file with the name counterClass.js.js which does not exist. So we need to import it without the .js extension.
 import './App.css';
+//although our counterClass name is like that ,when we impoert it we should use in as CounterClass and we should add component name as <CounterClass>
+//because in the counterClass.js file we are exporting it as CounterClass and we should use the same name when we import it in the App.js file. If we use a different name it will give an error because it will not find the component with that name. So we should use the same name as we exported in the counterClass.js file when we import it in the App.js file.
 import CounterClass from './components/counterClass';
 
 function App() {
---
+++ frontend/src/components/CounterFunction.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import CounterClass from './components/counterClass';
+
+function CounterFunction() {
+  return (
+    <div>
+      <h1>Counter Function Component</h1> 
+      </div>
+  );
+}
