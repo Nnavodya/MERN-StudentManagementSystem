da63555fc8b4a4d22c9f7d85622ffaf51e07a7b3
Nnavodya <rajapakse22030.kln@gmail.com>
2026-05-26 09:52:39 +0530

    fix: capitalize CounterClass import and render component in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 //When using import counterClass from './components/counterClass' ,don't put counterClass.js because it will automatically look for the file with the name counterClass.js in the components folder. If we put counterClass.js it will give an error because it will look for a file with the name counterClass.js.js which does not exist. So we need to import it without the .js extension.
 import './App.css';
-import counterClass from './components/counterClass';
+import CounterClass from './components/counterClass';
 
 function App() {
   return (
     <div className="App">
       <h1>Student Management System</h1>
-      <counterClass />
+      <CounterClass />
     </div>
   );
 }
