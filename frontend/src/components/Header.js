92f55f4be3534032a96c47f09074c04c76f5a972
Nnavodya <rajapakse22030.kln@gmail.com>
2026-05-27 10:06:08 +0530

    feat(Header): v1 - create basic navbar with brand and home link

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Link from 'react-router-dom/Link'; // Importing Link component for client-side navigation
 
 // Header component - Navigation bar for Student Management System
 function Header() {
@@ -11,7 +10,7 @@ function Header() {
     <Navbar expand="lg" variant="dark" style={{ backgroundColor: '#1a1a2e' }}>
       <Container>
 
-        {/* Brand/Logo - clicking takes to home page */}
+        {/* Brand/Logo */}
         <Navbar.Brand href="/">
           🎓 Student MS
         </Navbar.Brand>
@@ -21,23 +20,8 @@ function Header() {
 
         {/* Collapsible nav links */}
         <Navbar.Collapse id="student-ms-navbar">
-          <Nav className="ms-auto"> {/* ms-auto pushes links to the right */}
-
-            {/* Home link */}
-            <Nav.Link href="/">
-              🏠 Home
-            </Nav.Link>
-
-            {/* View all students link */}
-            <Nav.Link href="/students">
-              👨‍🎓 Students
-            </Nav.Link>
-
-            {/* Add new student link */}
-            <Nav.Link href="/add-student">
-              ➕ Add Student
-            </Nav.Link>
-
+          <Nav className="ms-auto">
+            <Nav.Link href="/">🏠 Home</Nav.Link>
           </Nav>
         </Navbar.Collapse>
 
