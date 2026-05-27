140185e815528d8cc13ab304fb08192b82823f95
Nnavodya <rajapakse22030.kln@gmail.com>
2026-05-27 07:54:46 +0530

    feat: add useEffect and state to fetch all students in AllStudents component

--- frontend/src/components/AllStudents.js
+++ frontend/src/components/AllStudents.js
@@ -1,1 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
+
+function AllStudents() {
+
+  const [students, setStudents] = useState([]); // State to hold the list of students
+
+}
+
+export default AllStudents; // Exporting the AllStudents component for use in other parts of the app
